4c5736829fca906ec546537705be7aa3bdbbd79a
Azswell <kurt.galanida@gmail.com>
2026-02-16 08:19:58 +0800

    Add project files.

---
+++ CPE223L_EXAM2_GALANIDA.PACLIBAR.MACUNTE/Form1.cs
@@ -0,0 +1,10 @@
+namespace CPE223L_EXAM2_GALANIDA.PACLIBAR.MACUNTE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Exam.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Exam.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
